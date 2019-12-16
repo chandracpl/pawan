8e6a15c418950783e4ff088c5f4c6f459b055c49
root <root@ip-172-31-91-146.ec2.internal>
2019-12-16 13:31:58 +0000

    pawan.com

---
+++ src/main/java/pawan/com/App.java
@@ -0,0 +1,13 @@
+package pawan.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
